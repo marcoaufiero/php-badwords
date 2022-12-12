10802dd2f8b689d515e4d9b21c326e3be9227016
marcoaufiero <marcoaufiero96@gmail.com>
2022-12-12 13:53:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+
+    $quote = "Voilà! Alla Vista un umile Veterano del Vaudeville,
+            chiamato a fare le Veci sia della Vittima che del Violento dalle Vicissitudini del fato. 
+            Questo Viso non è Vacuo Vessillo di Vanità, ma semplice Vestigia della Vox populi, ora Vuota, ora Vana. 
+            Tuttavia questa Visita alla Vessazione passata acquista Vigore ed è Votata alla Vittoria 
+            sui Vampiri Virulenti che aprono al Vizio, garanti della Violazione Vessatrice e Vorace della Volontà. 
+            L’unico Verdetto è Vendicarsi… Vendetta… E diventa un Voto non mai Vano
+            poiché il suo Valore e la sua Veridicità Vendicheranno un giorno coloro che sono Vigili e Virtuosi. 
+            In Verità questa Vichyssoise Verbale Vira Verso il Verboso,
+            quindi permettimi di aggiungere che è un grande onore per me conoscerti e che puoi chiamarmi V."
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h2>
+        <?php echo $quote ?>
+    </h2>
+
+
+</body>
+</html>
